42c93896de2105691961d494c5d46aa49d03e4c0
Valerio <valeriodevivo90@gmail.com>
2020-12-18 15:41:37 +0100

    Finish

---
+++ Es1/script.js
@@ -0,0 +1,25 @@
+// FOR __________________
+
+// var numero;
+// var somma = 0;
+
+// for (i=0; i < 5; i++) {
+//     numero = parseInt(prompt('inserisci un numero'));
+//     somma += numero;
+// }
+// console.log(somma);
+
+
+
+// WHILE ___________________
+
+var i=0;
+var somma = 0;
+
+while (i<5) {
+    var numero = parseInt(prompt('inserisci un numero'));
+    somma += numero;
+    i++;
+}
+
+console.log(somma);
